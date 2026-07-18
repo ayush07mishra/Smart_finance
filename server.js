37b4333ececfa6145ef234ec667bcf9383bb88b5
Ayush Mishra <143210488+ayush07mishra@users.noreply.github.com>
2026-07-19 00:52:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-
 require("dotenv").config({ path: "./.env" }); // force exact file
 
 const app = express();
